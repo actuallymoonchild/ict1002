5b1a01f52d8672d153edc1d4075a93a10be0be66
unknown <1604392i@gmail.com>
2021-10-17 22:22:08 +0800

    Updated Main

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ if __name__ == '__main__':
     from pprint import pprint
 
     # Gensim
-    import gensim
+    import gensim, spacy, warnings
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
     import gensim.corpora as corpora
     from gensim.utils import simple_preprocess
     from gensim.models import CoherenceModel
@@ -28,38 +29,39 @@ if __name__ == '__main__':
     nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
 
     # Plotting tools
-    import pyLDAvis
     import pyLDAvis.gensim_models
     import matplotlib.pyplot as plt
-
-    import warnings
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-
+    
     # NLTK Stopwords
     from nltk.corpus import stopwords
     stop_words = stopwords.words('english')
+    stop_words.extend(['got', 'say', 'use', 'from', 'gt', 'to'])
 
+    # Chapter 2: Importing Sample Data
     # Import Dataset
     df = pd.read_csv('redditandtweets.csv')
     df.head()
 
+    # Chapter 3: Cleaning and Tokenizing Sentences
     # Convert To List
     data = df.text.values.tolist()
 
-    # Remove Newline characters
-    data = [re.sub('\s+', ' ', text) for text in data]
-
-    # Remove distracting single quotes
-    data = [re.sub("\'", "", text) for text in data]
-
     # Remove Punctuations and Unnecessary characters
     def sent_to_words(sentences):
-        for sentence in sentences:
-            yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
+        for sent in sentences:
+            # Remove Newline characters
+            sent = re.sub('\s+', ' ', sent)
+
+            # Remove distracting single quotes
+            sent = re.sub("\'", "", sent)
+
+            # deacc=True removes punctuations
+            yield(gensim.utils.simple_preprocess(str(sent), deacc=True))
 
     data_words = list(sent_to_words(data))
 
-    # Build the bigram and tigram models
+    # Chapter 4: Building Bigram, Tigram Models and Lemmatize
+    # Build the bigram and trigram models
     bigram = gensim.models.Phrases(data_words, min_count = 5, threshold = 50)
     trigram = gensim.models.Phrases(bigram[data_words], threshold = 50)
 
@@ -68,53 +70,107 @@ if __name__ == '__main__':
     trigram_mod = gensim.models.phrases.Phraser(trigram)
 
     # Define functions for stopwords, bigrams, trigrams and lemmatization
-    def remove_stopwords(texts):
-        return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
-
-    def make_bigrams(texts):
-        return [bigram_mod[doc] for doc in texts]
-
-    def make_trigrams(texts):
-        return [trigram_mod[bigram_mod[doc]] for doc in texts]
-
-    def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
-        """https://spacy.io/api/annotation"""
+    def process_words(texts, stop_words=stop_words, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+        # Remove Stopwords, Form Bigrams, Trigrams and Lemmatization
+        texts = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+        texts = [bigram_mod[doc] for doc in texts]
+        texts = [trigram_mod[bigram_mod[doc]] for doc in texts]
         texts_out = []
+
         for sent in texts:
             doc = nlp(" ".join(sent)) 
             texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+
+        # remove stopwords once more after lemmatization
+        texts_out = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts_out]    
         return texts_out
 
-    # Remove Stop Words
-    data_words_nostops = remove_stopwords(data_words)
+    data_final = process_words(data_words) # Processed Text Data
 
-    # Form Bigrams
-    data_words_bigrams = make_bigrams(data_words_nostops)
-
-    # Do lemmatization keeping only noun, adj, vb, adv
-    data_lemmatized = lemmatization(data_words_bigrams)
-
+    # Chapter 5: Building the Topic Model
     # Create Dictionary
-    corpdict = corpora.Dictionary(data_lemmatized)
+    corpdict = corpora.Dictionary(data_final)
 
     # Create Corpus
-    texts = data_lemmatized
+    texts = data_final
 
     # Term Document Frequency
     corpus = [corpdict.doc2bow(text) for text in texts]
 
     # Build LDA Model
-    model = gensim.models.ldamodel.LdaModel(corpus = corpus, id2word = corpdict, num_topics=100, random_state=100, update_every=1, chunksize=100, passes=10, alpha='auto', per_word_topics=True)
+    model = gensim.models.ldamodel.LdaModel(corpus = corpus, id2word = corpdict, num_topics=20, random_state=100, update_every=1, chunksize=100, passes=10, alpha='auto', per_word_topics=True)
     doc_lda = model[corpus]
 
-    # Compute Perplexity
+    '''# Compute Perplexity
     print('\nPerplexity: ', model.log_perplexity(corpus))  # a measure of how good the model is. lower the better.
 
     # Compute Coherence Score
     coherence_model_lda = CoherenceModel(model=model, texts=data_lemmatized, dictionary=corpdict, coherence='c_v')
     coherence_lda = coherence_model_lda.get_coherence()
-    print('\nCoherence Score: ', coherence_lda)
+    print('\nCoherence Score: ', coherence_lda)'''
 
-    # Visualize the topics
-    vis = pyLDAvis.gensim_models.prepare(model, corpus, corpdict)
-    pyLDAvis.save_html(vis, 'visualization.html')
+    pprint(model.print_topics())
+
+    def visualiseTopics(ldamodel=None, corpus=corpus, texts = data):
+        # Initialize Output
+        sentdf = pd.DataFrame()
+
+        # Get main topic in each document
+        for i, rowlist in enumerate(ldamodel[corpus]):
+            row = rowlist[0] if ldamodel.per_word_topics else rowlist
+            row = sorted(row, key=lambda x: (x[1]), reverse=True)
+
+            for j, (topicnum, proptopic) in enumerate(row):
+                    if j == 0:
+                        wordprop = ldamodel.show_topic(topicnum)
+                        topickeywords = ", ".join([word for word, prop in wordprop])
+                        sentdf = sentdf.append(pd.Series([int(topicnum), round(proptopic,4), topickeywords]), ignore_index=True)
+                    else:
+                        break
+        sentdf.columns = ['Dominant_Topic', 'Percentage_Contribution', 'Topic_Keywords']
+        # Add original text to the end of the output
+        contents = pd.Series(texts)
+        sentdf = pd.concat([sentdf, contents], axis=1)
+        return(sentdf)
+
+    df_topic_sents_keywords = visualiseTopics(ldamodel=model, corpus=corpus, texts=texts)
+
+    # Format
+    df_dominant_topic = df_topic_sents_keywords.reset_index()
+    df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Percentage_Contrib', 'Keywords', 'Text']
+    df_dominant_topic.head(20)
+
+    '''from wordcloud import WordCloud, STOPWORDS
+    import matplotlib.colors as mcolors
+
+    cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+    cloud = WordCloud(stopwords=stop_words,
+                    background_color='white',
+                    width=2500,
+                    height=1800,
+                    max_words=10,
+                    colormap='tab10',
+                    color_func=lambda *args, **kwargs: cols[i],
+                    prefer_horizontal=1.0)
+
+    topics = model.show_topics(formatted=False)
+
+    fig, axes = plt.subplots(2, 2, figsize=(10,10), sharex=True, sharey=True)
+
+    for i, ax in enumerate(axes.flatten()):
+        fig.add_subplot(ax)
+        topic_words = dict(topics[i][1])
+        cloud.generate_from_frequencies(topic_words, max_font_size=300)
+        plt.gca().imshow(cloud)
+        plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+        plt.gca().axis('off')
+
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.axis('off')
+    plt.margins(x=0, y=0)
+    plt.tight_layout()
+    plt.show()'''
+
+
